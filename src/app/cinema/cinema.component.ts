84c5f9a618cd483e189c2d597b8f5513069dcbbf
RachidSAOUDI <rachid.saoudi@usmba.ac.ma>
2023-02-10 16:33:39 +0100

    display_film

--- src/app/cinema/cinema.component.ts
+++ src/app/cinema/cinema.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {CinemaService} from "../services/cinema.service";
+import {environment} from "../../environments/environment";
 
 @Component({
   selector: 'app-cinema',
@@ -11,6 +12,10 @@ export class CinemaComponent implements OnInit {
 
   public villes : any;
   public cinemas : any;
+  public salles : any;
+  public currentVille : any;
+  public currentCinema : any;
+  public host = environment.backhost;
 
   constructor(private cinemaService:CinemaService) { }
 
@@ -24,10 +29,30 @@ export class CinemaComponent implements OnInit {
   }
 
   onGetCinemas(v: any) {
-    this.cinemaService.getCinemas(v).subscribe(data=>{
+    this.currentVille=v;
+    this.cinemaService.getCinemas(v)
+      .subscribe(data=>{
       this.cinemas=data;
     },error =>{
       console.log(error);
     })
+  }
+
+  onGetSalles(c: any) {
+    this.currentCinema=c;
+    this.cinemaService.getSalles(c)
+      .subscribe(data=>{
+        this.salles=data;
+        this.salles._embedded.salles.forEach((salle:any)=>{
+          this.cinemaService.getPresentations(salle)
+            .subscribe((data:any)=>{
+              salle.presentations=data;
+          }, (err:any)=>{
+            console.log(err);
+          })
+        })
+      },error =>{
+        console.log(error);
+      })
   }
 }
--- src/app/services/cinema.service.ts
+++ src/app/services/cinema.service.ts
@@ -17,4 +17,13 @@ export class CinemaService {
   getCinemas(v:any) {
     return this.http.get(v._links.cinemas.href);
   }
+
+  getSalles(c: any) {
+    return this.http.get(c._links.salles.href);
+  }
+
+  getPresentations(salle:any) {
+    let url=salle._links.presentations.href.replace("{?projection}","");
+    return this.http.get(url+"?projection=p1")
+  }
 }
